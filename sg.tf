6c92ba90b8ddda7c35d640bf13b9336f464ae08b
kankhand <43746214+kankhand@users.noreply.github.com>
2023-06-02 12:10:55 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,8 @@
+resource "aws_security_group" "allow_tls" {
+    name = "day2-security-group"     ingress {
+        from_port = 443
+        to_port = 443
+         protocol = "tcp"
+         cidr_blocks = ["${aws_eip.lb1.public_ip}/32"]
+     }
+ }
